e5c94bbb5f3fb3536bcd46dfc4c313add0c6ab8c
fouadlizoullizoul <fouadlizoul22@gmail.com>
2024-12-14 10:45:36 +0100

    add middlwares fot loggin,security

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 const app = (0, express_1.default)();
 const PORT = 5000;
+app.use((0, morgan_1.default)('common'));
 //add routing for / path
 app.get('/', (req, res) => {
     res.json({ message: "Hi Res" });
--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,32 @@
 import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit';
 const app=express();
 const PORT =5000;
+
+app.use(morgan('common'))
+app.use(helmet());
+app.use(rateLimit({
+    windowMs: 60 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    legacyHeaders: false, 
+    standardHeaders:true,
+    message: 'Too many requests, please try again after an hour.'  // default message
+    
+}))
+app.use(express.json())
 
 //add routing for / path
 app.get('/',(req,res)=>{
     res.json({message:"Hi Res"})
 })
 
-
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    res.json({message:"Hello Fouad",data:req.body})
+})
 app.listen(PORT,()=>{
-    console.log("HI")
+    console.log("HI") 
 });
 export default app;
