2f984cc8fb4dd7cde2a3818464733c6e674b4a6e
fouadlizoullizoul <fouadlizoul22@gmail.com>
2024-12-16 19:40:22 +0100

    add validation middleware

---
+++ server/middleware/authentication.middleware.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import Error from "../interface/error.interface";
+import config from "../config";
+
+
+
+const handleUnauthorizedError = (next: NextFunction)=>{
+    const error: Error = new Error('Login Error:Please try again');
+    error.status = 401;
+    next(error);
+}
+const validateTokenMiddleware = (
+    req: Request,
+    _res: Response,
+    next: NextFunction
+) => {
+    try {
+        //get authHeader
+        const authHeader =req.get('Authorization');
+        if(authHeader){
+            const beare = authHeader.split(' ')[0].toLowerCase();
+            const token =authHeader.split(' ')[1];
+            if(token && beare){
+                const decode =jwt.verify(token, config.tokenSecret as unknown as string)
+                if(decode){
+                    next()
+                }else{
+                    handleUnauthorizedError(next)
+                }
+            }else{
+                //no token provided
+                handleUnauthorizedError(next);
+            }
+        }else{
+            //no token provider
+            handleUnauthorizedError(next);
+        }
+    } catch{
+        handleUnauthorizedError(next);
+    }
+}
+export default validateTokenMiddleware
--- server/routes/api/users.routes.ts
+++ server/routes/api/users.routes.ts
@@ -1,7 +1,8 @@
 import {Router} from 'express'
 import * as controllers from '../../controllers/users.controllers'
+import authenticationMiddlware from '../../middleware/authentication.middleware'
 const routes=Router()
-routes.route('/').get(controllers.getMany).post(controllers.create);
+routes.route('/').get(authenticationMiddlware,controllers.getMany).post(controllers.create);
 routes.route('/:id').get(controllers.getOne).patch(controllers.updateOne).delete(controllers.deleteOne);
 routes.route('/authenticate').post(controllers.authenticate)
 export default routes
